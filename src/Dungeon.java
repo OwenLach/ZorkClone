77c73cf80ae7a613537ce1c5229a00f0c6047641
Owen Lach <olach@mail.umw.edu>
2024-09-17 19:33:09 -0400

    Finished steps 3, 4, and 5

---
+++ src/Dungeon.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+
+public class Dungeon {
+   
+    private String title = "";
+    private Room entry;  // ask about
+    private Hashtable<String, Room> rooms = new Hashtable<String, Room>();
+    
+    public Dungeon(Room entry, String title) {
+        this.title = title;
+        this.entry = entry; // ask about 
+        this.add(entry);
+    }
+
+    public Room getEntry() { 
+        return this.entry;  
+    }
+    
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void add(Room room) {
+        this.rooms.put(room.getName(), room);
+    }
+
+    public Room getRoom(String roomname) {
+        return this.rooms.get(roomname);
+    }
+}
--- src/Interpreter.java
+++ src/Interpreter.java
@@ -2,7 +2,24 @@
 import java.util.*;
 
 class Interpreter {
+    
+    private static Dungeon buildSampleDungeon() {
+        Room entryRoom = new Room("entryRoom");
+        entryRoom.setDesc("This is a sample description of the Entry Room");
+
+        Dungeon sampleDungeon = new Dungeon(entryRoom, "Sample Dungeon");
+
+        Room r = sampleDungeon.getRoom("entryRoom");
+        System.out.println(r.describe());
+        
+        return sampleDungeon;
+    }
+
     public static void main(String args[]) {
+
+        Dungeon dungeon = Interpreter.buildSampleDungeon();
+
+
         Scanner scnr = new Scanner(System.in);
         char inputChar = ' '; 
 
