4e5a0b98cb7c6f84d5368a4d7ddf820e2b4cd1f1
Owen Lach <olach@mail.umw.edu>
2024-09-18 17:15:24 -0400

    might have fininshed step 9 correctly

---
+++ src/GameState.java
@@ -0,0 +1,47 @@
+
+import java.util.*;
+
+class GameState {
+
+    private static GameState instance = null;
+    private Dungeon dungeon = null;
+    private Room currRoom = null; 
+    private HashSet<Room> visited; 
+
+    public static GameState instance() {
+        if (GameState.instance == null) {
+            GameState.instance = new GameState();
+        } 
+        return GameState.instance;
+    }
+
+    private GameState() {
+        visited = new HashSet<>();
+    }
+
+    public void initialize(Dungeon dungeon) {
+        this.dungeon = dungeon;
+        this.currRoom = this.dungeon.getEntry();
+        visited.add(currRoom);
+    }
+
+    public Room getAdventurersCurrentRoom() {
+        return this.currRoom;
+    }
+
+    public void setAdventurersCurrentRoom(Room room) {
+        this.currRoom = room;
+    }
+
+    public Dungeon getDungeon() {
+        return this.dungeon;
+    }
+
+    void visit(Room room) {
+        visited.add(room);
+    }
+
+    boolean hasBeenVisited(Room room) {
+        return visited.contains(room);
+    }
+}
