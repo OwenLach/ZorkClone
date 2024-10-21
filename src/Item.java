65cd317fa02cc22e1fae64020bc7f5df64c184fa
Charlton Infinger <cinfinge@mail.umw.edu>
2024-10-21 12:19:57 -0400

    step two complete, using temp item.zork file. Not using aliases yet, nothing pointed to using it in step two.

--- src/Item.java
+++ src/Item.java
@@ -1,40 +1,85 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Scanner;
+import java.io.FileReader;
 
 public class Item {
 
+    
     private String primaryName = ""; 
-    private int weight = 0;
+    private int weight ;
     private Hashtable<String,String> messages = new Hashtable<String, String>();
     private HashSet<String> aliases = new HashSet<String>();
 
-    public Item(Scanner scnr) {
-    }
 
-    public boolean goesBy(String name) {
-    }
+    public Item(Scanner scnr) throws NoItemException{
+       
+		String firstLine = scnr.nextLine(); // skips "Items:"
 
-    public String getPrimaryName() { return this.primaryName; }
+		while(scnr.hasNextLine()) {
+			String itemName = scnr.nextLine();
+			if (itemName.equals("===")){
+            throw new NoItemException();
+         }
 
-    public String getMessageForVerb(String verb) {
-    }
+			this.primaryName = itemName; // item name is set to primary name
+			System.out.println("Item Name is " + itemName);
 
-    public String toString() {
-    }
+			//			String itemName = scan.nextLine(); // scans next line in file 
+			//			itemName.equals(this.primaryName); // sets line to item name
+			//			System.out.println(itemName); // prints out item name
+			//			
+			String itemWeight = scnr.nextLine();
+			this.weight = Integer.parseInt(itemWeight);
+			System.out.println("item weight is " + this.weight);
 
-    public int getWeight() { return weight; }
+			String itemAction;
+			boolean possibleAction = false; //used to verify if item is able to enact action
+			while(scnr.hasNextLine() && !(itemAction = scnr.nextLine().trim()).equals("---")) {
+				possibleAction = true;
+				String[] splitAction = itemAction.split(":");
+
+				if (splitAction.length ==2) { //checks to see if when line is split at ":" there is only 1 ":" verifying there is only two halves the key(left) and value(right)
+					String action = splitAction[0]; //sets what's before ":" as action name
+					String response = splitAction[1]; //what's after ":" as action response
+					messages.put(action, response); //adds the action and action response to hashtable
+					System.out.println("Action is " + action + " the ability " + response); //temp for texting
+				}
+			}
+			if(!possibleAction) { //checks to see if item has no action
+				System.out.println("this is a boring item"); //temp for testing
+			}
+		}
+	}
+
+	public boolean goesBy(String name) {
+		return aliases.contains(name);
+	}
+
+	public String getPrimaryName() {
+		return this.primaryName;
+	}
+	public String getMessageForVerb(String verb) {
+		return messages.getOrDefault(verb, ""); // returns the contents of the key that is called 
+	}
+
+	public String toString() {
+		return "Item " + primaryName + "'s weight is" + weight;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
 
 
-    public static void main(String args[]) {
-        //this main class is for testing 
-        //make a test file in the specified format
-        //read in from file 
-        //print out the inst vars to make sure its hydrated correctly
-        //
-        //maybe use .split(",") for the name and alieases
-        //
-        //for verb, maybe use .split(":") 
+	public static void main(String args[]) throws Exception {
+      try {
+         Scanner scan = new Scanner(new FileReader("../files/items.zork"));
+         Item item = new Item(scan);
+         scan.close(); // Close the scanner to avoid resource leaks
+      } catch (Exception e) {
+      }
 
-    }
-
+   }
 }
 
